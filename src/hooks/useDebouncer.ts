9b0d99b87e1e1e5a36a56f43a6741daebab41fff
FarazMehraien95 <farazmehraien95@gmail.com>
2024-07-17 20:40:38 +0330

    add useDebouncer hook

---
+++ src/hooks/useDebouncer.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+
+const useDebouncer = (value: string, delay: number): string => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value, delay])
+
+  return debouncedValue
+}
+
+export default useDebouncer
